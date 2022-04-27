6576b59ec524ae4942897e529876aed9a82f3943
Imran.Programmer <90908160+Imranprogrammer9@users.noreply.github.com>
2022-04-27 21:18:14 +0500

    Diagonal Diff

--- Diagonal Difference/diagonaldiff.cpp
+++ Diagonal Difference/diagonaldiff.cpp
@@ -6,7 +6,7 @@ string ltrim(const string &);
 string rtrim(const string &);
 vector<string> split(const string &);
 
-nt diagonalDifference(vector<vector<int>> arr) {
+int diagonalDifference(vector<vector<int>> arr) {
 
     int s1 = 0;
     int s2 = 0;
@@ -22,12 +22,12 @@ nt diagonalDifference(vector<vector<int>> arr) {
 
 int main()
 {
-    ofstream fout(getenv("OUTPUT_PATH"));
+    ofstream cout(getenv("OUTPUT_PATH"));
 
     string n_temp;
     getline(cin, n_temp);
 
-    int n = stoi(ltrim(rtrim(n_temp)));
+    int n = stdin(ltrim(rtrim(n_temp)));
 
     vector<vector<int>> arr(n);
 
@@ -40,7 +40,7 @@ int main()
         vector<string> arr_row_temp = split(rtrim(arr_row_temp_temp));
 
         for (int j = 0; j < n; j++) {
-            int arr_row_item = stoi(arr_row_temp[j]);
+            int arr_row_item = string(arr_row_temp[j]);
 
             arr[i][j] = arr_row_item;
         }
@@ -48,9 +48,9 @@ int main()
 
     int result = diagonalDifference(arr);
 
-    fout << result << "\n";
+    cout << result << "\n";
 
-    fout.close();
+    cout.close();
 
     return 0;
 }
